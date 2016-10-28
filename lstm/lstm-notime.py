114d8a6d66c854b8bf6082833eec5aa38318f4d1
gazkune <gorka.azkune@deusto.es>
2016-10-28 10:21:57 +0200

    Very simple LSTM model; timestamps are not taken into account

---
+++ lstm/lstm-notime.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 25 16:16:52 2016
+
+@author: gazkune
+"""
+
+from collections import Counter
+import json
+import sys
+
+import matplotlib.pyplot as plt
+
+from keras.models import Sequential
+from keras.models import model_from_json
+from keras.layers import Dense, Activation, Dropout
+from keras.layers import LSTM
+import numpy as np
+
+# Directory of datasets
+DIR = '../sensor2vec/kasteren_dataset/'
+# Dataset with vectors but without the action timestamps
+DATASET_NO_TIME = '../sensor2vec/kasteren_dataset/dataset_no_time.json'
+# List of unique actions in the dataset
+UNIQUE_ACTIVITIES = 'unique_activities.json'
+
+# Maximun number of actions in an activity
+ACTIVITY_MAX_LENGHT = 32
+# Number of dimensions of an action vector
+ACTION_MAX_LENGHT = 50
+
+
+def save_model(model):
+    json_string = model.to_json()
+    model_name = 'model_activity_lstm'
+    open(model_name + '.json', 'w').write(json_string)
+    model.save_weights(model_name + '.h5', overwrite=True)
+    
+def load_model(model_file, weights_file):
+    model = model_from_json(open(model_file).read())
+    model.load_weights(weights_file)
+    
+def check_activity_distribution(y_np, unique_activities):
+    activities = []
+    for activity_np in y_np:
+        index = activity_np.tolist().index(1.0)
+        activities.append(unique_activities[index])
+    print Counter(activities)
+    
+"""
+Function to plot accurary and loss during training
+"""
+
+def plot_training_info(metrics, save, history):
+    # summarize history for accuracy
+    if 'accuracy' in metrics:
+        
+        plt.plot(history.history['acc'])
+        plt.plot(history.history['val_acc'])
+        plt.title('model accuracy')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        if save == True:
+            plt.savefig('accuracy.png')
+            plt.gcf().clear()
+        else:
+            plt.show()
+
+    # summarize history for loss
+    if 'loss' in metrics:
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        #plt.ylim(1e-3, 1e-2)
+        plt.yscale("log")
+        plt.legend(['train', 'test'], loc='upper left')
+        if save == True:
+            plt.savefig('loss.png')
+            plt.gcf().clear()
+        else:
+            plt.show()
+        
+    
+def main(argv):    
+    unique_activities = json.load(open(UNIQUE_ACTIVITIES, 'r'))
+    total_activities = len(unique_activities)
+
+    print 'Preparing training set...'
+    print '  - Reading dataset'
+    sys.stdout.flush()
+    with open(DATASET_NO_TIME, 'r') as dataset_file:
+        activities = json.load(dataset_file)
+    print '  - processing activities'
+    X = []
+    y = []
+    for i, activity in enumerate(activities):
+        if i % 100 == 0:
+            print '  - Number of activities processed:', i
+            sys.stdout.flush()
+        actions = []
+        for action in activity['actions']:
+            actions.append(np.array(action))
+        X.append(np.array(actions))
+        y.append(np.array(activity['activity']))
+    
+    
+    total_examples = len(X)
+    test_per = 0.2
+    limit = int(test_per * total_examples)
+    X_train = X[limit:]
+    X_test = X[:limit]
+    y_train = y[limit:]
+    y_test = y[:limit]
+    print 'Total examples:', total_examples
+    print 'Train examples:', len(X_train), len(y_train) 
+    print 'Test examples:', len(X_test), len(y_test)
+    sys.stdout.flush()  
+    X = np.array(X_train)
+    y = np.array(y_train)
+    print 'Activity distribution for training:'
+    check_activity_distribution(y, unique_activities)
+    #X = X.reshape(X.shape[0], 1, ACTIVITY_MAX_LENGHT, ACTION_MAX_LENGHT)
+    X_test = np.array(X_test)
+    y_test = np.array(y_test)
+
+    print 'Activity distribution for testing:'
+    check_activity_distribution(y_test, unique_activities)
+
+    #X_test = X_test.reshape(X_test.shape[0], 1, ACTIVITY_MAX_LENGHT, ACTION_MAX_LENGHT) # For multichannel CNN
+    X = X.reshape(X.shape[0], ACTIVITY_MAX_LENGHT, ACTION_MAX_LENGHT)
+    X_test = X_test.reshape(X_test.shape[0], ACTIVITY_MAX_LENGHT, ACTION_MAX_LENGHT)
+    print 'Shape (X,y):'
+    print X.shape
+    print y.shape
+    print 'Training set prepared'  
+    sys.stdout.flush()
+
+
+    # Build the model
+
+    max_sequence_length = ACTIVITY_MAX_LENGHT
+    
+    print 'Building model...'
+    sys.stdout.flush()
+    model = Sequential()
+    model.add(LSTM(100, input_shape=(max_sequence_length, ACTION_MAX_LENGHT)))
+    model.add(Dropout(0.5))
+    model.add(Dense(total_activities))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', 'mse', 'mae'])
+    print 'Model built'
+    print(model.summary())
+    sys.stdout.flush()
+  
+
+    print 'Training...'
+    sys.stdout.flush()
+    history = model.fit(X, y, batch_size=50, nb_epoch=100, validation_data=(X_test, y_test))
+    print 'Saving model...'
+    sys.stdout.flush()
+    save_model(model)
+    print 'Model saved'
+    plot_training_info(['accuracy', 'loss'], True, history)
+
+
+if __name__ == "__main__":
+   main(sys.argv)
